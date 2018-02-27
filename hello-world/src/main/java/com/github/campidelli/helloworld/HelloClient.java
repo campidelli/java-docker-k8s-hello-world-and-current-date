fc80509e13c1aca6f301c67d83001bcbae78e0c9
campidelli <campidelli@gmail.com>
2018-02-27 17:08:13 -0300

    Using k8s environment variables to override those defined on application.yaml.

--- hello-world/src/main/java/com/github/campidelli/helloworld/HelloClient.java
+++ hello-world/src/main/java/com/github/campidelli/helloworld/HelloClient.java
@@ -2,6 +2,7 @@ package com.github.campidelli.helloworld;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -9,10 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class HelloClient {
 
+    @Value("${current-date.endpoint}")
+    private String endpoint;
+
     private RestTemplate restTemplate = new RestTemplate();
 
     public Date getCurrentDate() {
-        String endpoint = "http://current-date-service/current-date";
         return restTemplate.getForObject(endpoint, Date.class);
     }
 }
--- hello-world/src/main/java/com/github/campidelli/helloworld/HelloController.java
+++ hello-world/src/main/java/com/github/campidelli/helloworld/HelloController.java
@@ -3,11 +3,18 @@ package com.github.campidelli.helloworld;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+
+    @Value("${your.name}")
+    private String name;
+
+    @Value("${your.surname}")
+    private String surname;
 
     @Autowired
     private HelloClient helloClient;
@@ -15,6 +22,6 @@ public class HelloController {
     @RequestMapping("/hello-world")
     public String index() {
         Date today = helloClient.getCurrentDate();
-        return String.format("Hello World, today is %tD", today);
+        return String.format("Hello %s, %s. Today is %tD", surname, name, today);
     }
 }
